dcd903656fdd1146ede39e9647f488a0b5d57f0f
Shamnad PS <shamnad.shaji@gofore.com>
2018-09-20 23:06:50 +0300

    test

--- deploy-to-dev.sh
+++ deploy-to-dev.sh
@@ -1,5 +1,4 @@
 docker build --no-cache . -t typescript-project:latest
 docker tag typescript-project:latest registry.heroku.com/typescript-project/web:latest
-heroku container:login
 docker push registry.heroku.com/typescript-project/web
 heroku container:release web --app typescript-project
