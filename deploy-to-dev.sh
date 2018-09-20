6aa5087efa2620f4239c80d43029fb85a02f3734
Shamnad PS <shamnad.shaji@gofore.com>
2018-09-20 23:56:36 +0300

    update

--- deploy-to-dev.sh
+++ deploy-to-dev.sh
@@ -1,6 +1,3 @@
-heroku auth:token
 heroku update
-heroku help container
 heroku container:push web --app typescript-project
-heroku help container
 heroku container:release web --app typescript-project
