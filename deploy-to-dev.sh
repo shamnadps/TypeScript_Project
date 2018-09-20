f1e3582ed386997979653beb0fae16fe0ab06ab7
Shamnad PS <shamnad.shaji@gofore.com>
2018-09-20 23:27:42 +0300

    update

--- deploy-to-dev.sh
+++ deploy-to-dev.sh
@@ -1,6 +1,7 @@
-heroku container -v
+heroku help
+npm upgrade -g heroku
+heroku help
 heroku help container
 heroku container:push web --app typescript-project
-heroku container -v
 heroku help container
 heroku container:release web --app typescript-project
